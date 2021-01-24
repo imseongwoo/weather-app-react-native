fdaf8040bad177bfb4f459ee848eb6011953a99c
임성우 <seongwoorang@naver.com>
2021-01-24 21:41:26 +0900

    Create Loading.js

---
+++ Loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {StyleSheet,Text,View} from "react-native";
+
+export default function Loading(){
+    return <View style={styles.container}>
+        <Text>getting the weather</Text>
+    </View>
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"flex-end",
+        padding: 20
+    }
+})
