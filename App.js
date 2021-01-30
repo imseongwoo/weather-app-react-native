c2ccb526d7a30e3bc8de522bc9ae8ee6b7eadd5b
임성우 <seongwoorang@naver.com>
2021-01-30 23:10:33 +0900

    날씨
    날씨 가져오기

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+import * as Location from 'expo-location'
+
+const WEATHER_API_KEY = "57d0309882af17959f1c27a2c26991c2"
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hello!</Text>
-    </View>
-  );
-}
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitsSystem, setUnitsSystem] = useState('m');
+  useEffect(() => {
+    load()
+  }, [])
+  async function load() {
+    try {
+      let { status } = await Location.requestPermissionsAsync()
 
+      if (status !== 'granted') {
+        setErrorMessage('Access to location is needed to run the app')
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+
+      const { latitude, longitude } = location.coords
+
+      const weatherUrl = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+
+      const response = await fetch(weatherUrl)
+
+      const result = await response.json()
+
+      if (response.ok) {
+        setCurrentWeather(result)
+      } else {
+        setErrorMessage(result.message)
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+    }
+  }
+  if (currentWeather) {
+    const { main: { temp } } = currentWeather
+    return (
+      <View style={styles.container}>
+        <Text>{temp}</Text>
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+      </View>
+    )
+  }
+}
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"#fff",
-    alignItems:"center",
-    justifyContent:"center"
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
+
