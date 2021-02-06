b03d5efdbaa9456b50975552ba9b4ceed7f56bb1
임성우 <seongwoorang@naver.com>
2021-02-06 23:37:09 +0900

    Update App.js
    weatherinfo.js 추가

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Loading from "./Loading";
 import * as Location from 'expo-location'
+import WeatherInfo from './components/WeatherInfo'
 
 const WEATHER_API_KEY = "57d0309882af17959f1c27a2c26991c2"
-const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+
 
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [currentWeather, setCurrentWeather] = useState(null);
-  const [unitsSystem, setUnitsSystem] = useState('m');
+  const [unitsSystem, setUnitsSystem] = useState('metric');
   useEffect(() => {
     load()
   }, [])
@@ -18,21 +18,23 @@ export default function App() {
       let { status } = await Location.requestPermissionsAsync()
 
       if (status !== 'granted') {
-        setErrorMessage('Access to location is needed to run the app')
+        setErrorMessage('위치에 대한 접근이 필요합니다!')
         return
       }
       const location = await Location.getCurrentPositionAsync()
 
       const { latitude, longitude } = location.coords
 
-      const weatherUrl = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
-
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+      
       const response = await fetch(weatherUrl)
-
+      
       const result = await response.json()
 
       if (response.ok) {
         setCurrentWeather(result)
+        console.log(result)
+        
       } else {
         setErrorMessage(result.message)
       }
@@ -41,10 +43,14 @@ export default function App() {
     }
   }
   if (currentWeather) {
-    const { main: { temp } } = currentWeather
+    
     return (
       <View style={styles.container}>
-        <Text>{temp}</Text>
+        <View style={styles.main}>
+          <WeatherInfo currentWeather={currentWeather}/>
+        </View>
+
+        
       </View>
     )
   } else {
@@ -58,9 +64,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
     justifyContent: "center"
+  },
+  main:{
+    justifyContent:'center',
+    flex:1
   }
 });
 
