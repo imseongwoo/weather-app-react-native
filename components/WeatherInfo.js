6766f25f85b0c78c8f4a91e7cddd7283ba02a3f2
임성우 <seongwoorang@naver.com>
2021-02-11 13:44:11 +0900

    Update WeatherInfo.js

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -1,42 +1,46 @@
 import React from 'react'
-import { View, Text ,StyleSheet,Image} from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
-export default function WeatherInfo({currentWeather}) {
-    const {
-         main: { temp,temp_min,temp_max },
-         weather: [details]
+import { colors } from '../utils/index'
 
-} = currentWeather
-    const {icon,description} = details
+
+export default function WeatherInfo({ currentWeather }) {
+    const {
+        main: { temp, temp_min, temp_max },
+        weather: [details],
+        name
+
+    } = currentWeather
+    const { icon, description } = details
     const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
 
     return (
         <LinearGradient
-        // Background Linear Gradient
-        colors={['#2BC0E4','#EAECC6']}
-        style={styles.WeatherInfo}
+            // Background Linear Gradient
+            colors={['#2BC0E4', '#EAECC6']}
+            style={styles.WeatherInfo}
         >
-        {/* <View style={styles.WeatherInfo}> */}
-            <Image style = {styles.weatherIcon} source = {{uri:iconUrl}}></Image>
+            <Text>{name}</Text>
+            <Image style={styles.weatherIcon} source={{ uri: iconUrl }} />
             <Text>현재온도: {temp}</Text>
-            
+
             <Text>최대온도: {temp_max}</Text>
             <Text>최저온도: {temp_min}</Text>
-        {/* </View> */}
+
         </LinearGradient>
     )
 }
 
 const styles = StyleSheet.create({
-    WeatherInfo:{
-        alignItems:'center',
-        flex:1,
-        justifyContent:"center"
+    WeatherInfo: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: "center"
     },
-    weatherIcon:{
-        width:100,
-        height:100,
-        
-        
+    weatherIcon: {
+        width: 100,
+        height: 100,
+
+
     }
 })
